9b266c415bda4b1444d1bd830e055fa42891a872
2uy2 <quytran11204@gmail.com>
2026-02-01 13:45:48 +0700

    cú pháp sequelize

--- controller/client/tour_controller.ts
+++ controller/client/tour_controller.ts
@@ -4,8 +4,14 @@ import Tour from "../../models/tour_model";
 
 //get /tours
 export const index= async(req:Request,res:Response)=>{
+    // select * from tours 
+    // WHERE deleted = false AND status = "active"
     const tours = await Tour.findAll({
-        raw:false
+        raw:false,
+        where:{
+            deleted:false,
+            status:"active"
+        }
     })
     
     res.render("client/pages/tours/index",{
