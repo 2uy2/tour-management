f0bd83765c5d9dadfd520da0ef13191ccf43939f
2uy2 <quytran11204@gmail.com>
2026-02-02 10:45:31 +0700

    làm nut mini cart trên header

--- public/js/script.js
+++ public/js/script.js
@@ -1,31 +1,31 @@
 // slider tour detail
 var imagesThumb = new Swiper(".imagesThumb", {
-      spaceBetween: 10,
-      slidesPerView: 4,
-      freeMode: true,
-      watchSlidesProgress: true,
-    });
-    var imagesMain = new Swiper(".imagesMain", {
-      spaceBetween: 10,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      thumbs: {
-        swiper: imagesThumb,
-      },
-    });
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var imagesMain = new Swiper(".imagesMain", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: imagesThumb,
+  },
+});
 // end slider tour detail
 // alert add to success
-const alertAddCartSuccess=()=>{
+const alertAddCartSuccess = () => {
   const elementAlert = document.querySelector("[alert-add-cart-success]");
-  if(elementAlert){
+  if (elementAlert) {
     elementAlert.classList.remove("alert-hidden");
-    setTimeout(()=>{
+    setTimeout(() => {
       elementAlert.classList.add("alert-hidden");
-    },3000)
+    }, 3000)
     const closeAlert = elementAlert.querySelector("[close-alert]");
-    closeAlert.addEventListener("click",()=>{
+    closeAlert.addEventListener("click", () => {
       elementAlert.classList.add("alert-hidden");
     })
   }
@@ -34,32 +34,43 @@ const alertAddCartSuccess=()=>{
 // end alert add to sucess
 // cart
 const cart = localStorage.getItem("cart");
-if(!cart){
-  localStorage.setItem("cart",JSON.stringify([]));//chuyển dạng mảng thành dạng chuỗi
+if (!cart) {
+  localStorage.setItem("cart", JSON.stringify([])); //chuyển dạng mảng thành dạng chuỗi
 }
+//hiênt thị thêm số lượng vào mini cart
+const showMiniCart = () => {
+  const miniCart = document.querySelector("[mini-cart]");
+  if (miniCart) {
+    const cart = JSON.parse(localStorage.getItem("cart"));;
+    const totalQuantity = cart.reduce((sum, item) =>
+      sum + item.quantity, 0)
+    miniCart.innerHTML = totalQuantity;
+  }
+
+}
+showMiniCart();
 const formAddToCart = document.querySelector("[form-add-to-cart]");
-if(formAddToCart){
-  formAddToCart.addEventListener("submit",(event)=>{
-    event.preventDefault();//không bị nhảy trang
+if (formAddToCart) {
+  formAddToCart.addEventListener("submit", (event) => {
+    event.preventDefault(); //không bị nhảy trang
     const quantity = parseInt(formAddToCart.quantity.value)
     const tourId = parseInt(formAddToCart.getAttribute("tour-id"));
-    if(quantity >0 && tourId){
-      const cart  = JSON.parse(localStorage.getItem("cart"));
+    if (quantity > 0 && tourId) {
+      const cart = JSON.parse(localStorage.getItem("cart"));
       const indexExistTour = cart.findIndex(item => item.tourId == tourId);
-      if(indexExistTour == -1){
-          cart.push({
-            tourId:tourId,
-            quantity:quantity
-          });
+      if (indexExistTour == -1) {
+        cart.push({
+          tourId: tourId,
+          quantity: quantity
+        });
+      } else {
+        cart[indexExistTour].quantity = cart[indexExistTour].quantity + quantity
       }
-      else {
-        cart[indexExistTour].quantity = cart[indexExistTour].quantity+quantity
-      }
-    
-      localStorage.setItem("cart",JSON.stringify(cart));
+
+      localStorage.setItem("cart", JSON.stringify(cart));
     }
     alertAddCartSuccess();
-    
+    showMiniCart()
   })
 }
 // end cart
