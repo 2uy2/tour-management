2b817dd1e4fb3383c312c1068a08c49954f65574
2uy2 <quytran11204@gmail.com>
2026-02-02 09:18:06 +0700

    hiển thị hình ảnh dạng slide

--- controller/client/tour_controller.ts
+++ controller/client/tour_controller.ts
@@ -1,6 +1,7 @@
 import { Request,Response } from "express";
 import sequelize from "../../config.ts/database";
 import { json, QueryTypes } from "sequelize";
+import Tour from "../../models/tour_model";
 
 
 //get /tours/slugCategory
@@ -39,8 +40,23 @@ export const index= async(req:Request,res:Response)=>{
 //get /tours/detail/slugTour
 export const detail = async (req:Request,res:Response)=>{
     const slugTour = req.params.slugTour;
-    console.log(slugTour);
+    const tourDetail = await Tour.findOne({
+        where:{
+            slug:slugTour,
+            deleted:false,
+            status:"active"
+        },
+        raw:true
+    })
+
+    if(tourDetail["images"]){
+        tourDetail["images"]=JSON.parse(tourDetail["images"]);//chuyển dạng json thành mảng;
+    }
+    
+    tourDetail["price_special"]= tourDetail["price"]*(1-tourDetail["discount"]/100)
+    console.log(tourDetail)
     res.render("client/pages/tours/detail",{
-        pageTitle:"Chi tiết tour"
+        pageTitle:"Chi tiết tour",
+        tourDetail:tourDetail
     })
 }
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-import express , {Express,Request,Response} from "express";
+import express , {Express} from "express";
 import dotenv from "dotenv"
+import moment from "moment";
 import clientRoutes from "./routes/client/index_route";
 
 dotenv.config();
@@ -13,6 +14,9 @@ app.use(express.static("public"));
 //setting cho pug
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'pug');
+
+//app local variable
+app.locals.moment=moment
 
 //client routes
 clientRoutes(app);
