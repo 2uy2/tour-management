9a3ff8208c74604485a8e68e26edaf05acf07727
2uy2 <quytran11204@gmail.com>
2026-02-02 09:50:16 +0700

    thêm tour vào giỏ hàng

--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,33 @@ var imagesThumb = new Swiper(".imagesThumb", {
       },
     });
 // end slider tour detail
+// cart
+const cart = localStorage.getItem("cart");
+if(!cart){
+  localStorage.setItem("cart",JSON.stringify([]));//chuyển dạng mảng thành dạng chuỗi
+}
+const formAddToCart = document.querySelector("[form-add-to-cart]");
+if(formAddToCart){
+  formAddToCart.addEventListener("submit",(event)=>{
+    event.preventDefault();//không bị nhảy trang
+    const quantity = parseInt(formAddToCart.quantity.value)
+    const tourId = parseInt(formAddToCart.getAttribute("tour-id"));
+    if(quantity >0 && tourId){
+      const cart  = JSON.parse(localStorage.getItem("cart"));
+      const indexExistTour = cart.findIndex(item => item.tourId == tourId);
+      if(indexExistTour == -1){
+          cart.push({
+            tourId:tourId,
+            quantity:quantity
+          });
+      }
+      else {
+        cart[indexExistTour].quantity = cart[indexExistTour].quantity+quantity
+      }
+    
+      localStorage.setItem("cart",JSON.stringify(cart));
+    }
+    
+  })
+}
+// end cart
