7af30b584217ba168c6f678c7af9461ba78dc4ab
2uy2 <quytran11204@gmail.com>
2026-02-03 09:41:26 +0700

    đổ data ra trang in hoá đơn thành công

--- controller/client/order_controller.ts
+++ controller/client/order_controller.ts
@@ -7,33 +7,33 @@ import Tour from "../../models/tour_model";
 //post /order
 export const index = async (req: Request, res: Response) => {
 
-    const info = req.body.info;
-    const cart = req.body.cart;
+  const info = req.body.info;
+  const cart = req.body.cart;
 
 
-    // Lưu data vào bảng orders
-    
+  // Lưu data vào bảng orders
 
-    const order = await Order.create({
-        code: "",
-        fullName: info.fullName,
-        phone: info.phone,
-        note: info.note,
-        status: "initial",
-    });
-    const orderId = order.dataValues.id;
 
-    const code = generateOrderCode(orderId);
+  const order = await Order.create({
+    code: "",
+    fullName: info.fullName,
+    phone: info.phone,
+    note: info.note,
+    status: "initial",
+  });
+  const orderId = order.dataValues.id;
 
-    await Order.update({
-        code: code
-    }, {
-        where: {
-            id: orderId
-        }
-    });
+  const code = generateOrderCode(orderId);
 
-    // Lưu data vào bảng orders_items
+  await Order.update({
+    code: code
+  }, {
+    where: {
+      id: orderId
+    }
+  });
+
+  // Lưu data vào bảng orders_items
   for (const item of cart) {
     const dataItem = {
       orderId: orderId,
@@ -57,16 +57,56 @@ export const index = async (req: Request, res: Response) => {
     await OrderItem.create(dataItem);
   }
 
-    res.json({
-        code: "success",
-        message: "Đặt hàng thành công!",
-        orderCode: code
-    });
+  res.json({
+    code: "success",
+    message: "Đặt hàng thành công!",
+    orderCode: code
+  });
 };
 
 //get /order/success?orderCode=${data.orderCode}
 export const success = async (req: Request, res: Response) => {
+  const orderCode = req.query.orderCode; 
+
+  //tìm order đó
+  const order = await Order.findOne({
+    where: {
+      code: orderCode,
+      deleted: false,
+    },
+    raw: true,
+  });
+
+  //tìm chi tiết order
+  const ordersItem = await OrderItem.findAll({
+    where: {
+      orderId: order["id"],
+    },
+    raw: true,
+  });
+
+  for (const item of ordersItem) {
+    item["price_special"] = (item["price"] * (1 - item["discount"] / 100)); //giá sau khi giảm giá
+    item["total"] = item["price_special"] * item["quantity"]; //giá từng tour
+
+    const tourInfo = await Tour.findOne({
+      where: {
+        id: item["tourId"],
+      },
+      raw: true,
+    });
+
+    tourInfo["images"] = JSON.parse(tourInfo["images"]); //chuyển thành dạng array
+
+    item["image"] = tourInfo["images"][0]; //lấy ảnh đầu tiên làm thumbnail
+    item["title"] = tourInfo["title"];
+    item["slug"] = tourInfo["slug"];
+  }
+
+  order["total_price"] = ordersItem.reduce((sum, item) => sum + item["total"], 0); //tổng giá hoá đơn
   res.render("client/pages/order/success", {
-    pageTitle: "Đặt hàng thành công"
+    pageTitle: "Đặt hàng thành công",
+    order: order,
+    ordersItem: ordersItem
   })
 }
